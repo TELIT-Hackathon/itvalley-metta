1b0cba116e2d25c31403b6f7ac369addc11b79cf
FikoDe <deak.filip.21@gmail.com>
2022-12-03 18:47:48 +0100

    test pre dana

---
+++ appka/src/components/Home-page.vue
@@ -0,0 +1,16 @@
+<template>
+    <header-top/>
+</template>
+
+<script>
+import headerTop from './header-top.vue';
+    import TopMenu from './header-top.vue';
+    export default {
+  components: { headerTop },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
